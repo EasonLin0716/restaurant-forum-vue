76e6c07f09f7aa0273b944b20db810eae495bc35
Eason Lin <uu9924079@gmail.com>
2019-11-17 17:01:51 +0800

    add UserCommentsCard

---
+++ src/components/UserCommentsCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <strong>35</strong> 已評論餐廳
+    </div>
+    <div class="card-body">
+      <router-link
+        v-for="restaurant in user.FavoritedRestaurants"
+        :key="restaurant.id"
+        :to="{name: 'restaurant', params: { id: restaurant.id }}"
+      >
+        <img :src="restaurant.image | emptyImage" width="60" height="60" class="avatar" />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { emptyImageFilter } from "./../utils/mixins";
+export default {
+  name: "UserCommentsCard",
+  mixins: [emptyImageFilter],
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
--- src/views/User.vue
+++ src/views/User.vue
@@ -14,7 +14,7 @@
         <UserFollowersCard :user="user" />
       </div>
       <div class="col-md-8">
-        <!-- UserCommentsCard -->
+        <UserCommentsCard :user="user" />
       </div>
     </div>
   </div>
@@ -25,6 +25,7 @@
 import UserProfileCard from "./../components/UserProfileCard";
 import UserFollowingsCard from "./../components/UserFollowingsCard";
 import UserFollowersCard from "./../components/UserFollowersCard";
+import UserCommentsCard from "./../components/UserCommentsCard";
 
 const dummyData = {
   profile: {
@@ -1308,7 +1309,8 @@ export default {
   components: {
     UserProfileCard,
     UserFollowingsCard,
-    UserFollowersCard
+    UserFollowersCard,
+    UserCommentsCard
   },
   data() {
     return {
