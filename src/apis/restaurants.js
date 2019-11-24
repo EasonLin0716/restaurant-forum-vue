77348cc5940c3fd793dce0b91a65e1ccd14f732e
Eason Lin <uu9924079@gmail.com>
2019-11-24 12:57:47 +0800

    refactor api helper

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -1,32 +1,22 @@
 import { apiHelper } from './../utils/helpers'
-const getToken = () => localStorage.getItem('token')
 
 export default {
   // 攜帶 page 和 categoryId 參數
   getRestaurants({ page, categoryId }) {
     const searchParams = new URLSearchParams({ page, categoryId })
     // 透過 apiHelper 發出 GET 請求給 /restaurants
-    return apiHelper.get(`/restaurants?${searchParams.toString()}`, {
-      // 從 LocalStorage 取出 token，放進 Header
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/restaurants?${searchParams.toString()}`)
   },
 
   getRestaurant({ restaurantId }) {
-    return apiHelper.get(`/restaurants/${restaurantId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/restaurants/${restaurantId}`)
   },
 
   getFeeds() {
-    return apiHelper.get(`/restaurants/feeds`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/restaurants/feeds`)
   },
 
   getTopRestaurants() {
-    return apiHelper.get(`/restaurants/top`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/restaurants/top`)
   }
 }
--- src/apis/users.js
+++ src/apis/users.js
@@ -1,52 +1,33 @@
 import { apiHelper } from './../utils/helpers'
-const getToken = () => localStorage.getItem('token')
 
 export default {
   // 若 post 第二個參數不發送資料就傳 null
   addFavorite({ restaurantId }) {
-    return apiHelper.post(`/favorite/${restaurantId}`, null, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.post(`/favorite/${restaurantId}`, null)
   },
   deleteFavorite({ restaurantId }) {
-    return apiHelper.delete(`/favorite/${restaurantId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.delete(`/favorite/${restaurantId}`)
   },
   addLike({ restaurantId }) {
-    return apiHelper.post(`/like/${restaurantId}`, null, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.post(`/like/${restaurantId}`, null)
   },
   deleteLike({ restaurantId }) {
-    return apiHelper.delete(`/like/${restaurantId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.delete(`/like/${restaurantId}`)
   },
   // 回傳登入的 user json 資料
   getCurrentUser() {
-    return apiHelper.get(`/get_current_user`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/get_current_user`)
   },
   getTopUsers() {
-    return apiHelper.get('/users/top', {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get('/users/top')
   },
   get({ userId }) {
-    return apiHelper.get(`/users/${userId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/users/${userId}`)
   },
   addFollowing({ userId }) {
-    return apiHelper.post(`/following/${userId}`, null, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.post(`/following/${userId}`, null)
   },
   deleteFollowing({ userId }) {
-    return apiHelper.delete(`/following/${userId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.delete(`/following/${userId}`)
   }
 }
